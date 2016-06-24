e941ad180d54b7c79975851604d64342bb894e79
Lars Geyer-Blaumeiser <lars@blaumeiser-im-netz.de>
2016-06-24 19:42:39 +0200

    1: Create data model for ptm
    Task-Url: https://github.com/lgblaumeiser/ProjectTimeManager/issues/1

--- codebase/datamanager/src/test/java/de/lgblaumeiser/ptm/datamanager/model/BookingTest.java
+++ codebase/datamanager/src/test/java/de/lgblaumeiser/ptm/datamanager/model/BookingTest.java
@@ -5,19 +5,31 @@ package de.lgblaumeiser.ptm.datamanager.model;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalTime;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 /**
  * Test of the Booking class
  */
 public class BookingTest {
+	private static final LocalTime TIME1 = LocalTime.of(12, 34);
+	private static final LocalTime TIME2 = LocalTime.of(13, 57);
 
+	private static final Activity ACT1 = Activity.newLineActivity("Cat1", "0815");
+
+	private static final String COMMENT1 = "Test Comment\nTest Line 2";
 	/**
 	 * Positive test method for newBooking
 	 */
 	@Test
 	public final void testNewBookingLocalTimeActivity() {
-		//fail("Not yet implemented");
+		Booking testee = Booking.newBooking(TIME1, ACT1);
+		assertEquals(ACT1, testee.getActivity());
+		assertEquals(TIME1, testee.getStarttime());
+		assertFalse(testee.hasEndtime());
+		assertTrue(StringUtils.isBlank(testee.getComment()));
 	}
 
 	/**
@@ -25,7 +37,11 @@ public class BookingTest {
 	 */
 	@Test
 	public final void testNewBookingLocalTimeActivityString() {
-		//fail("Not yet implemented");
+		Booking testee = Booking.newBooking(TIME1, ACT1, COMMENT1);
+		assertEquals(ACT1, testee.getActivity());
+		assertEquals(TIME1, testee.getStarttime());
+		assertFalse(testee.hasEndtime());
+		assertEquals(COMMENT1, testee.getComment());
 	}
 
 	/**
@@ -33,7 +49,13 @@ public class BookingTest {
 	 */
 	@Test
 	public final void testEndBooking() {
-		//fail("Not yet implemented");
+		Booking startBooking = Booking.newBooking(TIME1, ACT1, COMMENT1);
+		Booking testee = Booking.endBooking(startBooking, TIME2);
+		assertEquals(ACT1, testee.getActivity());
+		assertEquals(TIME1, testee.getStarttime());
+		assertTrue(testee.hasEndtime());
+		assertEquals(TIME2, testee.getEndtime());
+		assertEquals(COMMENT1, testee.getComment());
 	}
 
 	/**
@@ -41,7 +63,11 @@ public class BookingTest {
 	 */
 	@Test
 	public final void testCalculateTimeSpan() {
-		//fail("Not yet implemented");
+		Booking startBooking = Booking.newBooking(TIME1, ACT1, COMMENT1);
+		Booking endBooking = Booking.endBooking(startBooking, TIME2);
+		TimeSpan testee = endBooking.calculateTimeSpan();
+		assertEquals(TIME1, testee.getStarttime());
+		assertEquals(TIME2, testee.getEndtime());
 	}
 
 }
