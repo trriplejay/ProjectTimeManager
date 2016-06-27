65d9d2db2ebf04dcd3fceefa14e7a72ca0ff3720
lars <lars@ubuntu>
2016-06-27 15:44:29 +0200

    2: Commit Functionality implemented

--- codebase/datamanager/src/main/java/de/lgblaumeiser/ptm/datamanager/service/DayBookingsImpl.java
+++ codebase/datamanager/src/main/java/de/lgblaumeiser/ptm/datamanager/service/DayBookingsImpl.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import java.util.SortedSet;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jdt.annotation.NonNull;
 
 import com.google.common.base.Preconditions;
@@ -24,127 +25,142 @@ import de.lgblaumeiser.ptm.datamanager.model.Booking;
  * The implementation of the DayBookings service
  */
 class DayBookingsImpl implements DayBookings {
-    private final @NonNull LocalDate day;
-    private final SortedSet<Booking> bookings = Sets.newTreeSet((A, B) -> A.getStarttime().compareTo(B.getStarttime()));
+	private final @NonNull LocalDate day;
+	private final SortedSet<Booking> bookings = Sets.newTreeSet((A, B) -> A.getStarttime().compareTo(B.getStarttime()));
 
-    /**
-     * Create a DayBooking service implementation
-     *
-     * @param day
-     *            The day for which bookings are stored.
-     */
-    DayBookingsImpl(@NonNull final LocalDate day) {
-	this.day = day;
-    }
-
-    @SuppressWarnings("null")
-    @Override
-    @NonNull
-    public Collection<@NonNull Booking> getBookings() {
-	return Collections.unmodifiableCollection(bookings);
-    }
-
-    @Override
-    public Booking getLastBooking() {
-	return Iterables.getLast(bookings, null);
-    }
-
-    @Override
-    @NonNull
-    public LocalDate getDay() {
-	return day;
-    }
-
-    @Override
-    @NonNull
-    public Booking addBooking(@NonNull final Activity activity) {
-	Preconditions.checkState(CollectionUtils.isNotEmpty(bookings));
-	Booking lastBooking = getLastBooking();
-	return addBooking(activity, lastBooking.getEndtime());
-    }
-
-    @Override
-    @NonNull
-    public Booking addBooking(@NonNull final Activity activity, @NonNull final LocalTime starttime) {
-	Booking lastBooking = getLastBooking();
-	if (lastBooking != null) {
-	    endBooking(lastBooking, starttime);
+	/**
+	 * Create a DayBooking service implementation
+	 *
+	 * @param day
+	 *            The day for which bookings are stored.
+	 */
+	DayBookingsImpl(@NonNull final LocalDate day) {
+		this.day = day;
 	}
-	Booking newBooking = Booking.newBooking(starttime, activity);
-	bookings.add(newBooking);
-	return newBooking;
-    }
 
-    @Override
-    @NonNull
-    public Booking endBooking(@NonNull final Booking booking, @NonNull final LocalTime endtime) {
-	Preconditions.checkState(bookings.remove(booking));
-	Preconditions.checkState(!booking.hasEndtime() || booking.getEndtime().equals(endtime));
-	Booking endedBooking = Booking.endBooking(booking, endtime);
-	bookings.add(endedBooking);
-	return endedBooking;
-    }
-
-    @Override
-    public void removeBooking(@NonNull final Booking booking) {
-	Preconditions.checkState(bookings.contains(booking));
-	final Booking previousBooking = getPreviousBooking(booking);
-	bookings.remove(booking);
-	if (previousBooking != null) {
-	    bookings.remove(previousBooking);
-	    Booking newBooking = Booking.newBooking(previousBooking.getStarttime(), previousBooking.getActivity(),
-		    previousBooking.getComment());
-	    if (booking.hasEndtime()) {
-		newBooking = Booking.endBooking(newBooking, booking.getEndtime());
-	    }
-	    bookings.add(newBooking);
+	@SuppressWarnings("null")
+	@Override
+	@NonNull
+	public Collection<@NonNull Booking> getBookings() {
+		return Collections.unmodifiableCollection(bookings);
 	}
-    }
 
-    private Booking getPreviousBooking(@NonNull final Booking booking) {
-	Set<Booking> previousElements = bookings.headSet(booking);
-	if (previousElements.isEmpty()) {
-	    return null;
+	@Override
+	public Booking getLastBooking() {
+		return Iterables.getLast(bookings, null);
 	}
-	return Iterables.getLast(previousElements);
-    }
 
-    @Override
-    public @NonNull Booking splitBooking(@NonNull final Booking booking, @NonNull final LocalTime splittime) {
-	// TODO Auto-generated method stub
-	return booking;
-    }
-
-    @Override
-    public @NonNull Booking changeBookingTimes(@NonNull final Booking booking, @NonNull final LocalTime starttime,
-	    @NonNull final LocalTime endtime) {
-	// TODO Auto-generated method stub
-	return booking;
-    }
-
-    @Override
-    public @NonNull Booking changeActivity(@NonNull final Booking booking, @NonNull final Activity activity) {
-	Preconditions.checkState(bookings.contains(booking));
-	Preconditions.checkState(!activity.equals(booking.getActivity()));
-	bookings.remove(booking);
-	Booking newBooking = Booking.newBooking(booking.getStarttime(), activity, booking.getComment());
-	if (booking.hasEndtime()) {
-	    newBooking = Booking.endBooking(newBooking, booking.getEndtime());
+	@Override
+	@NonNull
+	public LocalDate getDay() {
+		return day;
 	}
-	bookings.add(newBooking);
-	return newBooking;
-    }
 
-    @Override
-    public @NonNull Booking changeComment(@NonNull final Booking booking, @NonNull final String comment) {
-	// TODO Auto-generated method stub
-	return booking;
-    }
+	@Override
+	@NonNull
+	public Booking addBooking(@NonNull final Activity activity) {
+		Preconditions.checkState(CollectionUtils.isNotEmpty(bookings));
+		Booking lastBooking = getLastBooking();
+		return addBooking(activity, lastBooking.getEndtime());
+	}
 
-    @Override
-    public @NonNull Booking deleteComment(@NonNull final Booking booking) {
-	// TODO Auto-generated method stub
-	return booking;
-    }
+	@Override
+	@NonNull
+	public Booking addBooking(@NonNull final Activity activity, @NonNull final LocalTime starttime) {
+		Booking lastBooking = getLastBooking();
+		if (lastBooking != null) {
+			endBooking(lastBooking, starttime);
+		}
+		Booking newBooking = Booking.newBooking(starttime, activity);
+		bookings.add(newBooking);
+		return newBooking;
+	}
+
+	@Override
+	@NonNull
+	public Booking endBooking(@NonNull final Booking booking, @NonNull final LocalTime endtime) {
+		Preconditions.checkState(bookings.remove(booking));
+		Preconditions.checkState(!booking.hasEndtime() || booking.getEndtime().equals(endtime));
+		Booking endedBooking = Booking.endBooking(booking, endtime);
+		bookings.add(endedBooking);
+		return endedBooking;
+	}
+
+	@Override
+	public void removeBooking(@NonNull final Booking booking) {
+		Preconditions.checkState(bookings.contains(booking));
+		final Booking previousBooking = getPreviousBooking(booking);
+		bookings.remove(booking);
+		if (previousBooking != null) {
+			bookings.remove(previousBooking);
+			Booking newBooking = Booking.newBooking(previousBooking.getStarttime(), previousBooking.getActivity(),
+					previousBooking.getComment());
+			if (booking.hasEndtime()) {
+				newBooking = Booking.endBooking(newBooking, booking.getEndtime());
+			}
+			bookings.add(newBooking);
+		}
+	}
+
+	private Booking getPreviousBooking(@NonNull final Booking booking) {
+		Set<Booking> previousElements = bookings.headSet(booking);
+		if (previousElements.isEmpty()) {
+			return null;
+		}
+		return Iterables.getLast(previousElements);
+	}
+
+	@Override
+	public @NonNull Booking splitBooking(@NonNull final Booking booking, @NonNull final LocalTime splittime) {
+		// TODO Auto-generated method stub
+		return booking;
+	}
+
+	@Override
+	public @NonNull Booking changeBookingTimes(@NonNull final Booking booking, @NonNull final LocalTime starttime,
+			@NonNull final LocalTime endtime) {
+		// TODO Auto-generated method stub
+		return booking;
+	}
+
+	@Override
+	public @NonNull Booking changeActivity(@NonNull final Booking booking, @NonNull final Activity activity) {
+		Preconditions.checkState(bookings.contains(booking));
+		Preconditions.checkState(!activity.equals(booking.getActivity()));
+		bookings.remove(booking);
+		Booking newBooking = Booking.newBooking(booking.getStarttime(), activity, booking.getComment());
+		if (booking.hasEndtime()) {
+			newBooking = Booking.endBooking(newBooking, booking.getEndtime());
+		}
+		bookings.add(newBooking);
+		return newBooking;
+	}
+
+	@Override
+	public @NonNull Booking changeComment(@NonNull final Booking booking, @NonNull final String comment) {
+		Preconditions.checkState(bookings.contains(booking));
+		Preconditions.checkState(StringUtils.isNotBlank(comment));
+		Preconditions.checkState(!comment.equals(booking.getComment()));
+		bookings.remove(booking);
+		Booking newBooking = Booking.newBooking(booking.getStarttime(), booking.getActivity(), comment);
+		if (booking.hasEndtime()) {
+			newBooking = Booking.endBooking(newBooking, booking.getEndtime());
+		}
+		bookings.add(newBooking);
+		return newBooking;
+	}
+
+	@Override
+	public @NonNull Booking deleteComment(@NonNull final Booking booking) {
+		Preconditions.checkState(bookings.contains(booking));
+		Preconditions.checkState(StringUtils.isNotBlank(booking.getComment()));
+		bookings.remove(booking);
+		Booking newBooking = Booking.newBooking(booking.getStarttime(), booking.getActivity());
+		if (booking.hasEndtime()) {
+			newBooking = Booking.endBooking(newBooking, booking.getEndtime());
+		}
+		bookings.add(newBooking);
+		return newBooking;
+	}
 
 }
--- codebase/datamanager/src/test/java/de/lgblaumeiser/ptm/datamanager/service/DayBookingsTest.java
+++ codebase/datamanager/src/test/java/de/lgblaumeiser/ptm/datamanager/service/DayBookingsTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertTrue;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jdt.annotation.NonNull;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,185 +25,235 @@ import de.lgblaumeiser.ptm.datamanager.model.Booking;
  * Tests for the DayBookings service
  */
 public class DayBookingsTest {
-    @SuppressWarnings("null")
-    private static final @NonNull LocalDate DATE1 = LocalDate.of(2016, 06, 24);
-    @SuppressWarnings("null")
-    private static final @NonNull LocalTime TIME1 = LocalTime.of(10, 12);
-    @SuppressWarnings("null")
-    private static final @NonNull LocalTime TIME2 = LocalTime.of(12, 15);
-    @SuppressWarnings("null")
-    private static final @NonNull LocalTime TIME3 = LocalTime.of(13, 45);
-    private static final @NonNull Activity ACTIVITY1 = Activity.newLineActivity("a", "b");
-    private static final @NonNull Activity ACTIVITY2 = Activity.newProjectActivity("a1", "c");
-    private static final @NonNull Booking BOOKING1 = Booking.newBooking(TIME1, ACTIVITY1);
+	@SuppressWarnings("null")
+	private static final @NonNull LocalDate DATE1 = LocalDate.of(2016, 06, 24);
+	@SuppressWarnings("null")
+	private static final @NonNull LocalTime TIME1 = LocalTime.of(10, 12);
+	@SuppressWarnings("null")
+	private static final @NonNull LocalTime TIME2 = LocalTime.of(12, 15);
+	@SuppressWarnings("null")
+	private static final @NonNull LocalTime TIME3 = LocalTime.of(13, 45);
+	private static final @NonNull Activity ACTIVITY1 = Activity.newLineActivity("a", "b");
+	private static final @NonNull Activity ACTIVITY2 = Activity.newProjectActivity("a1", "c");
+	private static final @NonNull Booking BOOKING1 = Booking.newBooking(TIME1, ACTIVITY1);
+	private static final @NonNull String COMMENT1 = "My Comment";
+	
+	private DayBookings testee;
 
-    private DayBookings testee;
+	@Before
+	public void before() {
+		testee = DayBookings.createDayBookings(DATE1);
+	}
 
-    @Before
-    public void before() {
-	testee = DayBookings.createDayBookings(DATE1);
-    }
+	@Test
+	public void testCreateDayBookings() {
+		assertEquals(DATE1, testee.getDay());
+		assertTrue(testee.getBookings().isEmpty());
+	}
 
-    @Test
-    public void testCreateDayBookings() {
-	assertEquals(DATE1, testee.getDay());
-	assertTrue(testee.getBookings().isEmpty());
-    }
+	/**
+	 * Test unmodifiability of booking list
+	 */
+	@Test(expected = UnsupportedOperationException.class)
+	public void testBookingListUnmodifiability() {
+		testee.getBookings().add(BOOKING1);
+	}
 
-    /**
-     * Test unmodifiability of booking list
-     */
-    @Test(expected = UnsupportedOperationException.class)
-    public void testBookingListUnmodifiability() {
-	testee.getBookings().add(BOOKING1);
-    }
+	@Test
+	public void testGetLastBooking() {
+		assertNull(testee.getLastBooking());
+	}
 
-    @Test
-    public void testGetLastBooking() {
-	assertNull(testee.getLastBooking());
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testAddBooking0Empty() {
+		testee.addBooking(ACTIVITY1);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testAddBooking0Empty() {
-	testee.addBooking(ACTIVITY1);
-    }
+	@Test
+	public void testAddBooking0Positive() {
+		Booking firstOne = testee.addBooking(ACTIVITY1, TIME1);
+		testee.endBooking(firstOne, TIME2);
+		Booking testBooking = testee.addBooking(ACTIVITY2);
+		assertFalse(testee.getBookings().isEmpty());
+		assertEquals(2, testee.getBookings().size());
+		assertEquals(testBooking, testee.getLastBooking());
+		assertEquals(TIME2, testee.getLastBooking().getStarttime());
+		assertFalse(testee.getLastBooking().hasEndtime());
+	}
 
-    @Test
-    public void testAddBooking0Positive() {
-	Booking firstOne = testee.addBooking(ACTIVITY1, TIME1);
-	testee.endBooking(firstOne, TIME2);
-	Booking testBooking = testee.addBooking(ACTIVITY2);
-	assertFalse(testee.getBookings().isEmpty());
-	assertEquals(2, testee.getBookings().size());
-	assertEquals(testBooking, testee.getLastBooking());
-	assertEquals(TIME2, testee.getLastBooking().getStarttime());
-	assertFalse(testee.getLastBooking().hasEndtime());
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testAddBooking0NoEndtime() {
+		testee.addBooking(ACTIVITY1, TIME1);
+		testee.addBooking(ACTIVITY2);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testAddBooking0NoEndtime() {
-	testee.addBooking(ACTIVITY1, TIME1);
-	testee.addBooking(ACTIVITY2);
-    }
+	@Test
+	public void testAddBooking1Empty() {
+		Booking booking = testee.addBooking(ACTIVITY1, TIME1);
+		assertFalse(testee.getBookings().isEmpty());
+		assertEquals(1, testee.getBookings().size());
+		assertEquals(booking, testee.getLastBooking());
+		assertFalse(testee.getLastBooking().hasEndtime());
+	}
 
-    @Test
-    public void testAddBooking1Empty() {
-	Booking booking = testee.addBooking(ACTIVITY1, TIME1);
-	assertFalse(testee.getBookings().isEmpty());
-	assertEquals(1, testee.getBookings().size());
-	assertEquals(booking, testee.getLastBooking());
-	assertFalse(testee.getLastBooking().hasEndtime());
-    }
+	@Test
+	public void testAddBooking1With2Bookings() {
+		testee.addBooking(ACTIVITY1, TIME1);
+		Booking testBooking = testee.addBooking(ACTIVITY2, TIME2);
+		assertEquals(2, testee.getBookings().size());
+		assertEquals(testBooking, testee.getLastBooking());
+		assertFalse(testee.getLastBooking().hasEndtime());
+		assertTrue(Iterables.getFirst(testee.getBookings(), BOOKING1).hasEndtime());
+	}
 
-    @Test
-    public void testAddBooking1With2Bookings() {
-	testee.addBooking(ACTIVITY1, TIME1);
-	Booking testBooking = testee.addBooking(ACTIVITY2, TIME2);
-	assertEquals(2, testee.getBookings().size());
-	assertEquals(testBooking, testee.getLastBooking());
-	assertFalse(testee.getLastBooking().hasEndtime());
-	assertTrue(Iterables.getFirst(testee.getBookings(), BOOKING1).hasEndtime());
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testAddBooking1WithEndedLastBooking() {
+		Booking firstOne = testee.addBooking(ACTIVITY1, TIME1);
+		testee.endBooking(firstOne, TIME2);
+		testee.addBooking(ACTIVITY2, TIME3);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testAddBooking1WithEndedLastBooking() {
-	Booking firstOne = testee.addBooking(ACTIVITY1, TIME1);
-	testee.endBooking(firstOne, TIME2);
-	testee.addBooking(ACTIVITY2, TIME3);
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testAddBooking1WithSameStarttime() {
+		testee.addBooking(ACTIVITY1, TIME1);
+		testee.addBooking(ACTIVITY2, TIME1);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testAddBooking1WithSameStarttime() {
-	testee.addBooking(ACTIVITY1, TIME1);
-	testee.addBooking(ACTIVITY2, TIME1);
-    }
+	@Test
+	public void testEndBooking() {
+		Booking booking = testee.addBooking(ACTIVITY1, TIME1);
+		Booking result = testee.endBooking(booking, TIME2);
+		assertFalse(testee.getBookings().isEmpty());
+		assertEquals(1, testee.getBookings().size());
+		assertEquals(result, testee.getLastBooking());
+		assertTrue(testee.getLastBooking().hasEndtime());
+	}
 
-    @Test
-    public void testEndBooking() {
-	Booking booking = testee.addBooking(ACTIVITY1, TIME1);
-	Booking result = testee.endBooking(booking, TIME2);
-	assertFalse(testee.getBookings().isEmpty());
-	assertEquals(1, testee.getBookings().size());
-	assertEquals(result, testee.getLastBooking());
-	assertTrue(testee.getLastBooking().hasEndtime());
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testEndBookingSecondEnd() {
+		Booking booking = testee.addBooking(ACTIVITY1, TIME1);
+		Booking endedBooking = testee.endBooking(booking, TIME2);
+		testee.endBooking(endedBooking, TIME3);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testEndBookingSecondEnd() {
-	Booking booking = testee.addBooking(ACTIVITY1, TIME1);
-	Booking endedBooking = testee.endBooking(booking, TIME2);
-	testee.endBooking(endedBooking, TIME3);
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testEndBookingWrongBookingTime() {
+		Booking testBooking = testee.addBooking(ACTIVITY1, TIME2);
+		testee.endBooking(testBooking, TIME1);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testEndBookingWrongBookingTime() {
-	Booking testBooking = testee.addBooking(ACTIVITY1, TIME2);
-	testee.endBooking(testBooking, TIME1);
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testEndBookingWithAnUnknownBooking() {
+		testee.endBooking(BOOKING1, TIME3);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testEndBookingWithAnUnknownBooking() {
-	testee.endBooking(BOOKING1, TIME3);
-    }
+	@Test
+	public void testRemoveBooking() {
+		Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
+		testee.removeBooking(testBooking);
+		assertTrue(testee.getBookings().isEmpty());
+	}
 
-    @Test
-    public void testRemoveBooking() {
-	Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
-	testee.removeBooking(testBooking);
-	assertTrue(testee.getBookings().isEmpty());
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testRemoveBookingUnknownBooking() {
+		testee.removeBooking(BOOKING1);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testRemoveBookingUnknownBooking() {
-	testee.removeBooking(BOOKING1);
-    }
+	@Test
+	public void testRemoveBookingWithPreviousBooking() {
+		testee.addBooking(ACTIVITY1, TIME1);
+		Booking startBooking = testee.addBooking(ACTIVITY2, TIME2);
+		Booking testBooking = testee.endBooking(startBooking, TIME3);
+		testee.removeBooking(testBooking);
+		assertEquals(1, testee.getBookings().size());
+		assertEquals(TIME3, testee.getLastBooking().getEndtime());
+		assertEquals(TIME1, testee.getLastBooking().getStarttime());
+	}
 
-    @Test
-    public void testRemoveBookingWithPreviousBooking() {
-	testee.addBooking(ACTIVITY1, TIME1);
-	Booking startBooking = testee.addBooking(ACTIVITY2, TIME2);
-	Booking testBooking = testee.endBooking(startBooking, TIME3);
-	testee.removeBooking(testBooking);
-	assertEquals(1, testee.getBookings().size());
-	assertEquals(TIME3, testee.getLastBooking().getEndtime());
-	assertEquals(TIME1, testee.getLastBooking().getStarttime());
-    }
+	@Test
+	public void testRemoveBookingWithPreviousBookingButUnendedRemoveCandidate() {
+		testee.addBooking(ACTIVITY1, TIME1);
+		Booking testBooking = testee.addBooking(ACTIVITY2, TIME2);
+		testee.removeBooking(testBooking);
+		assertEquals(1, testee.getBookings().size());
+		assertEquals(TIME1, testee.getLastBooking().getStarttime());
+		assertFalse(testee.getLastBooking().hasEndtime());
+	}
 
-    @Test
-    public void testRemoveBookingWithPreviousBookingButUnendedRemoveCandidate() {
-	testee.addBooking(ACTIVITY1, TIME1);
-	Booking testBooking = testee.addBooking(ACTIVITY2, TIME2);
-	testee.removeBooking(testBooking);
-	assertEquals(1, testee.getBookings().size());
-	assertEquals(TIME1, testee.getLastBooking().getStarttime());
-	assertFalse(testee.getLastBooking().hasEndtime());
-    }
+	@Test
+	public void testRemoveBookingFirst() {
+		testee.addBooking(ACTIVITY1, TIME1);
+		testee.addBooking(ACTIVITY2, TIME2);
+		testee.removeBooking(Iterables.getFirst(testee.getBookings(), BOOKING1));
+		assertEquals(1, testee.getBookings().size());
+		assertEquals(TIME2, testee.getLastBooking().getStarttime());
+	}
 
-    @Test
-    public void testRemoveBookingFirst() {
-	testee.addBooking(ACTIVITY1, TIME1);
-	testee.addBooking(ACTIVITY2, TIME2);
-	testee.removeBooking(Iterables.getFirst(testee.getBookings(), BOOKING1));
-	assertEquals(1, testee.getBookings().size());
-	assertEquals(TIME2, testee.getLastBooking().getStarttime());
-    }
+	@Test
+	public void testChangeActivity() {
+		Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
+		testee.changeActivity(testBooking, ACTIVITY2);
+		assertEquals(1, testee.getBookings().size());
+		assertEquals(ACTIVITY2, testee.getLastBooking().getActivity());
+	}
 
-    @Test
-    public void testChangeActivity() {
-	Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
-	testee.changeActivity(testBooking, ACTIVITY2);
-	assertEquals(1, testee.getBookings().size());
-	assertEquals(ACTIVITY2, testee.getLastBooking().getActivity());
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testChangeActivityWithUnknownBooking() {
+		testee.changeActivity(BOOKING1, ACTIVITY2);
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testChangeActivityWithUnknownBooking() {
-	testee.changeActivity(BOOKING1, ACTIVITY2);
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testChangeActivityWithSameActivity() {
+		Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
+		testee.changeActivity(testBooking, ACTIVITY1);
+	}
+	
+	@Test
+	public void testChangeComment() {
+		Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
+		testee.changeComment(testBooking, COMMENT1);
+		assertEquals(COMMENT1, testee.getLastBooking().getComment());
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testChangeCommentWithUnknownBooking() {
+		testee.changeComment(BOOKING1, COMMENT1);
+	}
+	
+	@SuppressWarnings("null")
+	@Test(expected = IllegalStateException.class)
+	public void testChangeCommentWithEmptyComment() {
+		Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
+		testee.changeComment(testBooking, StringUtils.EMPTY);
+	}
+	
+	@SuppressWarnings("null")
+	@Test(expected = IllegalStateException.class)
+	public void testChangeCommentTwiceTheSame() {
+		Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
+		testee.changeComment(testBooking, COMMENT1);		
+		testee.changeComment(testee.getLastBooking(), COMMENT1);
+	}
+	
+	@SuppressWarnings("null")
+	@Test
+	public void testDeleteComment() {
+		testee.addBooking(ACTIVITY1, TIME1);
+		testee.changeComment(testee.getLastBooking(), COMMENT1);
+		assertEquals(COMMENT1, testee.getLastBooking().getComment());
+		testee.deleteComment(testee.getLastBooking());
+		assertTrue(StringUtils.isBlank(testee.getLastBooking().getComment()));
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testChangeActivityWithSameActivity() {
-	Booking testBooking = testee.addBooking(ACTIVITY1, TIME1);
-	testee.changeActivity(testBooking, ACTIVITY1);
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testDeleteCommentWithUnknownBooking() {
+		testee.deleteComment(BOOKING1);
+	}
+	
+	@SuppressWarnings("null")
+	@Test(expected = IllegalStateException.class)
+	public void testDeleteCommentAlthoughNoComment() {
+		testee.addBooking(ACTIVITY1, TIME1);
+		testee.deleteComment(testee.getLastBooking());		
+	}
 }
