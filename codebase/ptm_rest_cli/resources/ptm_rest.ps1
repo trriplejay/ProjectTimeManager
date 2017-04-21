d883dab6cfc6c9c089668eb83480905e2529bf01
Lars Geyer-Blaumeiser <lgblaumeiser@gmail.com>
2017-04-21 08:43:38 +0200

    8: Script for starting and ending server and client.
    Task-Url: http://github.com/lgblaumeiser/ProjectTimeManager/issues/8

---
+++ codebase/ptm_rest_cli/resources/ptm_rest.ps1
@@ -0,0 +1,8 @@
+$Process = [Diagnostics.Process]::Start("java","-jar ptm_rest-0.0.1-SNAPSHOT.jar")
+$id = $Process.Id
+Start-Sleep -Seconds 10
+[Diagnostics.Process]::Start("java","-jar ptm_rest_cli-1.0.0-SNAPSHOT-jar-with-dependencies.jar").WaitForExit()
+try {            
+    Stop-Process -Id $id -ErrorAction stop            
+} catch {            
+}
