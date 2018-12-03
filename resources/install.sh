353b9f79b7be3a11d24a390dceba4b673d63ce75
Lars Geyer-Blaumeiser <lgblaumeiser@gmail.com>
2018-12-03 19:31:51 +0100

    Prepare new release

--- resources/install.sh
+++ resources/install.sh
@@ -10,8 +10,8 @@
 
 echo "Step: Request installation data"
 
-cli_jar=ptm_cli-1.2.jar
-rest_jar=ptm_rest-1.2.jar
+cli_jar=ptm_cli-1.4.jar
+rest_jar=ptm_rest-1.4.jar
 
 read -p "Installation folder: " installfolder
 
